836ffe2ea5d0caeb544a43212da1057e1133d27d
TacoJimmy <t5669016@ntut.org.tw>
2022-03-18 04:07:26 +0800

    Update sendinfo.py

--- sendinfo.py
+++ sendinfo.py
@@ -9,14 +9,16 @@ def on_publish():
     print(AC_Status[5])
     if (AC_Status[5] == 1):
         client = mqtt.Client()
-        client.username_pw_set("GAruSGOZYNeLWtbGD9D5","xxxx")
+        client.username_pw_set("Nr8bhLdynDUe85WM9kBV","xxxx")
         client.connect('thingsboard.cloud', 1883, 60)
         payload = {'airconditiongstatus' : AC_Status[0],'operationmode' : AC_Status[1],'windspeed' : AC_Status[2],'settemperature' : AC_Status[3], 'roomtemperature' : AC_Status[4]  }
         #print (json.dumps(payload))
         client.publish("v1/devices/me/telemetry", json.dumps(payload))
         time.sleep(1)
 
+'''
 while True:
     
     on_publish()
     time.sleep(10)
+'''
