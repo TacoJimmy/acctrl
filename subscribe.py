9dd8f1ef6bca3800f12511f7799c905344477780
TacoJimmy <t5669016@ntut.org.tw>
2022-03-17 17:56:31 +0800

    Create subscribe.py

---
+++ subscribe.py
@@ -0,0 +1,39 @@
+
+# coding:utf-8
+import codecs
+import json
+import ssl
+import paho.mqtt.client as mqtt
+import time
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code"+str(rc))
+    client.subscribe('v1/devices/me/rpc/request/+',1)
+    time.sleep(3)
+
+def on_message(client, userdata, msg):
+    data_topic = msg.topic
+    data_payload = json.loads(msg.payload.decode())
+    time.sleep(.5)
+    listtopic = data_topic.split("/")  
+    signal_fb = 'v1/devices/me/rpc/response/'+str(listtopic[5]) # response topic
+    fb_payload = {'good': 1}
+    print(data_payload)
+    client.publish(signal_fb,json.dumps(fb_payload)) # send respose
+
+def ipc_subscribe(token):
+    meter_token = "GAruSGOZYNeLWtbGD9D5"
+    meter_pass = ''
+    url = 'thingsboard.cloud'
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    time.sleep(.5)
+    client.on_message = on_message
+    time.sleep(.5)
+    
+    client.username_pw_set(meter_token, meter_pass)
+    client.connect(url, 1883, 60)
+
+
+    client.loop_forever()
